ebc141b9b818f4cf11c6c40b1223b1369df9b57d
zebraoflove <zebra.titarenko@yandex.ru>
2024-08-13 01:49:27 +0700

    Delete App.test.tsx

--- src/App.test.tsx
+++
@@ -1,8 +0,0 @@
-// import MainApp from './App';
-// import {createRoot} from "react-dom/client";
-// test('renders starts without crashing', () => {
-//   const div = document.createElement("div")
-//   const root = createRoot(div);
-//   root.render(<MainApp/>)
-//   root.unmount()
-// });
